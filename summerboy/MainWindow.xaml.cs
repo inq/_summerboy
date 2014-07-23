0202c74e698bc7b9527e5109b907a6897f23d6b3
inkyu <gofiri@gmail.com>
2014-07-24 02:14:53 +0900

    implemented drawing canvas

--- summerboy/MainWindow.xaml.cs
+++ summerboy/MainWindow.xaml.cs
@@ -20,6 +20,9 @@ namespace summerboy
     /// </summary>
     public partial class MainWindow : Window
     {
+        private WriteableBitmap wb;
+        private int r, g, b;
+
         public MainWindow()
         {
             InitializeComponent();
@@ -32,11 +35,48 @@ namespace summerboy
 
         private void Window_Initialized(object sender, EventArgs e)
         {
+            r = 150;
+            g = 100;
+            b = 100;
+
+            var image = new BitmapImage(new Uri(""));
+            wb = new WriteableBitmap(image.PixelWidth, image.PixelHeight, image.DpiX, image.DpiY, PixelFormats.Bgra32, null);
             log("initialized.");
             this.SizeToContent = SizeToContent.Width;
             imageBox.BeginInit();
-            imageBox.Source = new BitmapImage(new Uri(""));
+            imageBox.Source = image;
             imageBox.EndInit();
+            surveyBox.BeginInit();
+            surveyBox.Source = wb;
+            surveyBox.EndInit();
+        }
+
+        private void imageBox_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            Point p = e.GetPosition(this);
+            p.X *= wb.PixelWidth / imageBox.ActualWidth;
+            p.Y *= wb.PixelWidth / imageBox.ActualWidth;
+            log("" + p.X + "," + p.Y);
+            
+            wb.Lock();
+            unsafe
+            {
+                for (int i = (int)p.Y - 30; i - p.Y < 30 && i < wb.PixelHeight; i++)
+                {
+                    for (int j = (int)p.X - 30; j - p.X < 30 && j < wb.PixelWidth; j++)
+                    {
+                        int pBackBuffer = (int)wb.BackBuffer + (i * wb.BackBufferStride) + j * 4;
+                        int color_data = 128 << 24 | r << 16 | g << 8 | b;
+                        *((int *)pBackBuffer) |= color_data;
+
+                    }
+                }
+            }
+
+            r += 10;
+            
+            wb.AddDirtyRect(new Int32Rect(0, 0, wb.PixelWidth, wb.PixelHeight));
+            wb.Unlock();
         }
     }
 }
